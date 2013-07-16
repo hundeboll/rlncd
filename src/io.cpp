a68e6771407f46483a0e26c21c163d1ad1e15cc5
Martin Hundebøll <martin@hundeboll.net>
2013-07-16 14:03:28 +0200

    fix argument to check_ge

--- src/io.cpp
+++ src/io.cpp
@@ -248,7 +248,7 @@ void io::write_thread()
 
         if (m_nlsock) {
             VLOG(LOG_IO) << "send message";
-            CHECK_GE(nl_send_auto(m_nlsock, msg, 0));
+            CHECK_GE(nl_send_auto(m_nlsock, msg), 0);
         }
 
         LOG(INFO) << "free message: " << prio;
